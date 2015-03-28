ec7f5c68fffc215a6a753236930ab600c5a4ebb1
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-03-28 00:13:45 -0500

    Finished interactive functionality

--- js/index_script.js
+++ js/index_script.js
@@ -1,23 +1,9 @@
-/* Requirements:
-	1. Object to store questions. (this.questions)
-	2. Array as long as number of questions (this.totalQuestions)
-	3. Variable to store the number of correct answer (this.totalCorrect)
-	4. Function to change the button appearance & text based on where the user is
-	5. Function to display the appropriate view
-	6. Function to check the answer
-	7. Function to decide the feedback
-	8. Function to change the question
-	9. Function to roll up the explanation
-	10. Function that binds event handlers
-		a. button click to next
-		b. button click to submit
-		c. clicking an answer (check)
-*/
-
 // Initialize a blank object
 var quizApp = {};
 
+// =================
 // QUESTION DATA SET
+// =================
 quizApp.questions = [
 	{
 		"topic": "HTML",
@@ -26,7 +12,7 @@ quizApp.questions = [
 					"&#60div&#62",
 					"&#60img&#62",
 					"&#60button&#62"],
-		"answer": 2,
+		"answer": 3,
 		"explanation": "&#60img&#62 is one of the self-closing tags. Usually, you would write it as &#60img src=\”some_url.pic\” /&#60. See the list of self-closing tags right here.",
 		"image": ""
 	},
@@ -37,47 +23,49 @@ quizApp.questions = [
 					"section.header {background: red;}",
 					"section#header {background: red;}",
 					"section {background: red;}"],
-		"answer": 2,
+		"answer": 3,
 		"explanation": "ID selectors, \"#\" are quite specific. In most cases it trumps class selectors, \".\", and plain tag name selector such as \"section\".",
 		"image": ""
 	},
 	{
 		"topic": "jQuery",
-		"question": "Question about jQuery",
-		"choices": ["choice 1",
-					"choice 2",
-					"choice 3",
-					"choice 4"],
-		"answer": 2,
-		"explanation": "Explaining the answer for jQuery",
+		"question": "You've been using a lot of \"$()\" method in jQuery. A lot of JavaScript is done behind the scene for you when you use that method. What does \"$()\" method do?",
+		"choices": ["Selects DOM element(s)",
+					"Declares function(s)",
+					"Initiates loop(s)",
+					"Converts string to integer"],
+		"answer": 1,
+		"explanation": "\"$()\" method selects DOM element(s). It is the equivalent of document.querySelector() in vanilla JavaScript. However, the jQuery method or function works out browswer inconsitencies and legacy issues for the programmer.",
 		"image": ""
 	},
 	{
 		"topic": "JavaScript",
-		"question": "Question about JavaScript",
-		"choices": ["choice 1",
-					"choice 2",
-					"choice 3",
-					"choice 4"],
-		"answer": 2,
-		"explanation": "Explaining the answer for JavaScript",
+		"question": "Good understanding of programming concepts is also a must-have for a front-end developer. In JavaScript, every object has a prototype chain that can be traced all the way back to this.",
+		"choices": ["prototype",
+					"Object",
+					"undefined",
+					"null"],
+		"answer": 4,
+		"explanation": "\"null\" is the ultimate prototype of every object in JavaScript. It has no prototype of itself. JavaScript is the programming language behind a lot of popular professional framework. Familiarity with JavaScript will help you learn other popular technologies.",
 		"image": ""
 	},
 	{
 		"topic": "Git",
-		"question": "Question about Git",
-		"choices": ["choice 1",
-					"choice 2",
-					"choice 3",
-					"choice 4"],
+		"question": "Version Control is crucial in any software or web developemtn. GitHub is just one of the many tools out there that helps us manage different versions of our work. What would be the correct flow in saving your work on GitHub?",
+		"choices": ["init -> pull -> merge -> add",
+					"add -> commit -> push -> merge",
+					"add -> pull -> push -> commit",
+					"commit -> merge -> push -> add"],
 		"answer": 2,
-		"explanation": "Explaining the answer for Git",
+		"explanation": "After initializing both local and remote repository, you would normally \"add\" -> \"commit\" -> \"push\" to the branch that you're working on. If you're satisfied with the result, you can merge it to the master branch to publish your work.",
 		"image": ""
 	}
 ];
 
 
+// =================
 // VARIABLES
+// =================
 quizApp.totalStages = quizApp.questions.length + 1;
 quizApp.currentQuestion = 0;
 quizApp.totalCorrect = 0;
@@ -85,9 +73,12 @@ quizApp.userChoice = -1;
 quizApp.buttonText = "Start";
 
 
+// =================
 // METHODS
+// =================
+// Attach event handlers to elements
 quizApp.bindUI = function() {
-	// Handle click events on the multiple choice
+	// Attach click handler on the multiple choice
 	var ul = document.getElementById("choices")
 	var li = ul.getElementsByTagName("li");
 	for (var i = 0; i < li.length; i++) {
@@ -95,7 +86,7 @@ quizApp.bindUI = function() {
 		li[i].addEventListener("click", setUserChoice, false);
 	}
 
-	// Handle click events for button
+	// Attach click handler for button
 	var button = document.getElementById("button");
 	button.addEventListener("click", processClick, false);
 
@@ -109,13 +100,14 @@ quizApp.bindUI = function() {
 		e.target.className = "selected";
 	}
 
-	// Store which answer that user clicks
+	// Store which answer user clicks
 	function setUserChoice(e) {
 		console.log("setUserChoice fired")
 		quizApp.userChoice = e.target.id;
 		console.log(quizApp.userChoice);
 	}
 
+	// CHECKPOINT OF THE FLOW
 	// Decides what to do when user clicks the blue button
 	function processClick() {
 		console.log("processClick fired");
@@ -145,6 +137,7 @@ quizApp.bindUI = function() {
 				quizApp.currentQuestion = 1;
 				quizApp.totalCorrect = 0;
 				quizApp.userChoice = -1;
+				quizApp.resetAnswer();
 				quizApp.buttonText = "Submit";
 				quizApp.updateButton(quizApp.buttonText);
 				quizApp.render();
@@ -200,6 +193,8 @@ quizApp.render = function() {
 	 }
 
 	// Show question slide
+	// Reset user's choice
+	// Update current question and total question on the HTML
 	function renderQuestions() {
 		quizApp.loadQuestion();
 		if (quizApp.currentQuestion >= 2) {
@@ -208,10 +203,14 @@ quizApp.render = function() {
 		document.getElementById("start").className = "hidden";
 		document.getElementById("result").className = "hidden";
 		document.getElementById("questions").className = "";
+		document.getElementById("current_question").innerHTML = quizApp.currentQuestion;
+		document.getElementById("total_question").innerHTML = quizApp.questions.length;
 	}
 };
 
 // Check wether or not user's answer is correct
+// Tell user if it's correct or incorrect
+// Show the explanation
 quizApp.processInput = function() {
 	console.log("processInput fired");
 	console.log("userChoice is " + quizApp.userChoice);
@@ -236,35 +235,32 @@ quizApp.updateButton = function(text) {
 	document.getElementById("button_text").innerHTML = text;
 };
 
-// Display appropriate button style according to the text
+// Set appropriate button style according to the text
 quizApp.renderButton = function() {
 	console.log ("rendering button");
-	switch (quizApp.buttonText) {
-		case "Start":
-			document.getElementById("button_container").style.width = "100%";
-			document.getElementById("button_container").className = ""; /* First time, button was invisible */
-			break;
-		case "Submit":
-			document.getElementById("button_container").style.width = "80%";
-			break;
-		case "Next":
-			document.getElementById("button_text").innerHTML = text;
-			break;
-		case "Start over":
-			document.getElementById("button_container").style.width = "100%";
-			document.getElementById("button_container").className = "";
-			break;
-		default:
-			console.log("Invalid text for button.");
+	if (quizApp.buttonText == "Start" || quizApp.buttonText == "Start over")
+	{
+		document.getElementById("button_container").style.width = "100%";
+		document.getElementById("button_container").className = ""; /* First time, button was invisible */
+	}
+	else if (quizApp.buttonText == "Submit" || quizApp.buttonText == "Next")
+	{
+		document.getElementById("button_container").style.width = "80%";
+	}
+	else
+	{
+		console.log("Invalid text for button.");
 	}
 };
 
 // Display explanation by modifying its style
+// Update the score
 quizApp.renderExplanation = function() {
 	console.log("rendering explanation");
 	document.getElementById("explanation").style.top = "0";
-	document.getElementById("feedback").className = "";
-	document.getElementById("explanation_content").className = "";
+	document.getElementById("feedback").className = "visible";
+	document.getElementById("explanation_content").className = "visible";
+	document.getElementById("current_score").innerHTML = quizApp.totalCorrect;
 };
 
 // Put explanation out of sight
@@ -297,10 +293,25 @@ quizApp.loadQuestion = function() {
 	}
 }
 
+// Display total score at the end
+// Gives out recommendation based on the score
 quizApp.getScore = function() {
-	// Compute the final score
 	document.getElementById("total_score").innerHTML = quizApp.totalCorrect;
-	// NEXT: Print recommendation based on score
+	var ratio = quizApp.totalCorrect / quizApp.questions.length;
+	var recommendation;
+	if (ratio == 1)
+		recommendation = "Perfect! You're definitely ready to move to the next unit.";
+	else if (ratio >= 0.8)
+		recommendation = "Good job. Feel free to move to the next unit.";
+	else if (ratio >= 0.6)
+		recommendation = "Not too bad. Try again to see if you can score higher this time.";
+	else if (ratio >= 0.4)
+		recommendation = "Don't worry, maybe reviewing the material one more time will help solidify things.";
+	else if (ratio >= 0.2)
+		recommendation = "Consider reviewing the material at least one more time.";
+	else
+		recommendation = "Ouch! It seems like you went over the material too fast. Do it slower this time.";
+	document.getElementById("recommendation").innerHTML = recommendation;
 }
 
 
